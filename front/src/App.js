16f26797f2e09f2465c49611a391206f461acfea
Gabriel Cabanes <gabriel.cabanes0@gmail.com>
2020-02-15 16:16:56 +0100

    absolute path

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Test from 'components/Test';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit&nbsp;
+          <code>src/App.js</code>
+          &nbsp;and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Test />
     </div>
   );
 }
---
+++ front/src/components/Test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test = () => {
+  return (<div>WEWE</div>);
+};
+
+export default Test;
